ff491d89e12d926f21a87554ef915c4ab1349d67
Qihong Liu <qihong.liu@hotmail.com>
2021-06-11 20:58:13 +0800

    feasible

--- src/predictor.c
+++ src/predictor.c
@@ -7,6 +7,7 @@
 //========================================================//
 #include <stdio.h>
 #include "predictor.h"
+#include <string.h>
 
 //
 // TODO:Student Information
@@ -46,22 +47,42 @@ uint8_t tPredition; // tournament branch prediction
 
 // gshare
 uint32_t globalHistory;
-uint32_t *globalBHT;
+uint8_t *globalBHT;
 
 
 //
 //TODO: Add your own Branch Predictor data structures here
 //
 // perceptron
+/*
 uint8_t pPrediction;
 uint32_t perceptron_num;
-int **weights;
+uint32_t **weights;
 uint32_t weight_num;
 uint8_t *history;
 uint32_t threshold;
 uint32_t history_len;
+*/
+uint32_t nbits;
+int **weights;
+uint32_t nweights;
+uint32_t nperceptrons;
+uint32_t histlength;
+int8_t *history;
+uint32_t threshold;
+uint32_t predictor_size;
+uint32_t pcmask;
 
-
+uint32_t
+make_mask(uint32_t size)
+{
+  uint32_t mmask = 0;
+  for(int i=0;i<size;i++)
+  {
+    mmask = mmask | 1<<i;
+  }
+  return mmask;
+}
 
 
 //------------------------------------//
@@ -75,17 +96,17 @@ uint32_t history_len;
 void init_gshare_predictor(){
 	// init globalHistory and global Branch History Table
 	globalHistory = 0;
-	int bitsNeed = sizeof(uint32_t) << pcIndexBits;
-	globalBHT = malloc(bitsNeed);
+	int bitsNeed = sizeof(uint8_t) * (1<< pcIndexBits);
+	globalBHT = malloc(sizeof(uint8_t) * (1<< pcIndexBits));
 	if(globalBHT==NULL){
 		fprintf(stderr, "[ERROR] [Malloc] global branch history table failed.\n");
 	    exit(-1);
 	}
-	memset(globalBHT, WN, bitsNeed);
-	printf("Gshare initialization finished.")
+	memset(globalBHT, WN, sizeof(uint8_t) * (1<< pcIndexBits));
+	printf("Gshare initialization finished.");
 }
 
-uint8_t make_gshare_prediction(pc){
+uint8_t make_gshare_prediction(uint32_t pc){
 	// compute index
 	uint32_t bitMask = (1 << ghistoryBits) - 1;
 	uint32_t globalBHT_index = (pc ^ globalHistory) & bitMask;
@@ -94,19 +115,24 @@ uint8_t make_gshare_prediction(pc){
 	else return TAKEN;
 }
 
-void train_gshare_predictor(pc, outcome){
+void train_gshare_predictor(uint32_t pc, uint8_t outcome){
 	// compute index
 	uint32_t bitMask = (1 << ghistoryBits) - 1;
-	uint32_t globalBHT_index = (pc ^ globalHistory) & bitMask;
-	// update globalHistory
-	globalHistory = (globalHistory << 1) + outcome;
+	//uint32_t globalBHT_index = (pc ^ globalHistory) & bitMask;
+  uint32_t globalBHT_index = (globalHistory ^ pc) & bitMask;
 	// 2-bit predict
 	if(outcome == TAKEN){
-		if(globalBHT[globalBHT_index] < ST) globalBHT[globalBHT_index] += 1;
+		if(globalBHT[globalBHT_index] < ST){
+      globalBHT[globalBHT_index] ++;
+    }
 	}
 	else{
-		if(globalBHT[globalBHT_index] > SN) globalBHT[globalBHT_index] -= 1;
+		if(globalBHT[globalBHT_index] > SN){
+      globalBHT[globalBHT_index] --;
+    }
 	}
+  // update globalHistory
+  globalHistory = (globalHistory << 1) | outcome;
 }
 
 //------------------------------------//
@@ -115,7 +141,7 @@ void train_gshare_predictor(pc, outcome){
 
 void init_tournament_predictor(){
   // Initialize Local History Table
-  lht=(uint8_t*)malloc((1<<pcIndexBits)*sizeof(uint8_t));
+  lht=(uint32_t*)malloc((1<<pcIndexBits)*sizeof(uint32_t));
   if(lht==NULL){
     fprintf(stderr, "[ERROR] [Malloc] local history table failed.\n");
     exit(-1);
@@ -152,11 +178,11 @@ void init_tournament_predictor(){
   gPredition=WN;
   lPredition=WN;
 
-  printf("tournament initialization finished.")
+  //printf("tournament initialization finished.");
 }
 
 uint8_t make_local_prediction(uint32_t pc){
-  uint32_t lbtPred=lbt[lht[pc & (1<<pcIndexBits -1)]];
+  uint32_t lbtPred=lbt[lht[pc & ((1<<pcIndexBits) -1)]];
   if(lbtPred == WN || lbtPred == SN){
     return NOTTAKEN;
   }
@@ -166,7 +192,7 @@ uint8_t make_local_prediction(uint32_t pc){
 }
 
 uint8_t make_global_prediction(uint32_t pc){
-  uint32_t gbtPred=gbt[ghr & (1<<ghistoryBits -1)];
+  uint32_t gbtPred=gbt[ghr & ((1<<ghistoryBits) -1)];
   if(gbtPred == WN || gbtPred == SN){
     return NOTTAKEN;
   }
@@ -176,7 +202,7 @@ uint8_t make_global_prediction(uint32_t pc){
 }
 uint8_t tournament_choose(uint8_t localPred, uint8_t globalPred, uint32_t pc){
   uint8_t choice = cbt[ghr & ((1<<ghistoryBits)-1)];
-  return (choice == WN || choice == SN) ? globalResult : localResult;
+  return (choice == WN || choice == SN) ? globalPred : localPred;
 }
 
 uint8_t make_tournament_prediction(uint32_t pc){
@@ -188,33 +214,33 @@ uint8_t make_tournament_prediction(uint32_t pc){
 void train_local_predictor(uint32_t pc, uint8_t outcome) {
   // update lbt
   if(outcome==NOTTAKEN){
-    if(lbt[lht[pc & (1<<pcIndexBits -1)]]!=SN){
-      lbt[lht[pc & (1<<pcIndexBits -1)]]--;
+    if(lbt[lht[pc & ((1<<pcIndexBits) -1)]]!=SN){
+      lbt[lht[pc & ((1<<pcIndexBits) -1)]]--;
     }
   }
   else{
-    if(lbt[lht[pc & (1<<pcIndexBits -1)]]!=ST){
-      lbt[lht[pc & (1<<pcIndexBits -1)]]++;
+    if(lbt[lht[pc & ((1<<pcIndexBits) -1)]]!=ST){
+      lbt[lht[pc & ((1<<pcIndexBits) -1)]]++;
     }
   }
 
   // update lht
-  lht[pc & (1<<pcIndexBits -1)] <<=1 ;
-  lht[pc & (1<<pcIndexBits -1)] &= ((1 << lhistoryBits) - 1);
-  lht[pc & (1<<pcIndexBits -1)] |= outcome;
+  lht[pc & ((1<<pcIndexBits) -1)] <<=1 ;
+  lht[pc & ((1<<pcIndexBits) -1)] &= ((1 << lhistoryBits) - 1);
+  lht[pc & ((1<<pcIndexBits) -1)] |= outcome;
 
 }
 
 void train_global_predictor(uint32_t pc, uint8_t outcome) {
   // update gbt
   if(outcome==NOTTAKEN){
-    if(gbt[ghr & (1<<ghistoryBits -1)]!=SN){
-      gbt[ghr & (1<<ghistoryBits -1)]--;
+    if(gbt[ghr & ((1<<ghistoryBits) -1)]!=SN){
+      gbt[ghr & ((1<<ghistoryBits) -1)]--;
     }
   }
   else{
-    if(gbt[ghr & (1<<ghistoryBits -1)]!=ST){
-      gbt[ghr & (1<<ghistoryBits -1)]++;
+    if(gbt[ghr & ((1<<ghistoryBits) -1)]!=ST){
+      gbt[ghr & ((1<<ghistoryBits) -1)]++;
     }
   }
   // update ghr
@@ -227,8 +253,8 @@ void train_chooser_predictor(uint32_t pc, uint8_t outcome) {
   uint8_t localOutcome = make_local_prediction(pc);
   uint8_t globalOutcome = make_global_prediction(pc);
 
-  uint32_t lbtPred=lbt[lht[pc & (1<<pcIndexBits -1)]];
-  uint32_t gbtPred=ghr[ghr & (1<<ghistoryBits -1)];
+  uint32_t lbtPred=lbt[lht[pc & ((1<<pcIndexBits) -1)]];
+  uint32_t gbtPred=gbt[ghr & ((1<<ghistoryBits) -1)];
   uint8_t choice = cbt[ghr & ((1<<ghistoryBits)-1)];
 
   if(localOutcome!=globalOutcome){
@@ -255,6 +281,7 @@ void train_tournament_predictor(uint32_t pc, uint8_t outcome){
 //------------------------------------//
 
 void init_custom_predictor(){
+  /*
 	history_len = 128;
 	perceptron_num = 413436;
 	weight_num = history_len + 1;
@@ -266,7 +293,7 @@ void init_custom_predictor(){
 		fprintf(stderr, "[ERROR] [Malloc] history failed.\n");
 	    exit(-1);
 	}
-	memset(history, 0, bitsNeed);
+	memset(history, 0, bitNeed);
 	// init weights matrix (perceptronNumber, weightsNumber)
 	weights = (uint32_t**)malloc(sizeof(uint32_t*) * perceptron_num);
 	if(weights==NULL){
@@ -281,33 +308,82 @@ void init_custom_predictor(){
 		}
 		memset(weights[i], 0, sizeof(uint32_t) * weight_num); 
 	}
+  */
+  int size;
+  //ghist = 0;
+  //gmask = (1<<ghistoryBits -1);
+  //lmask = (1<<lhistoryBits -1);
+
+  nbits = 12;
+  predictor_size = 640000000;
+  histlength = 128;
+  pcmask = make_mask(nbits);
+  nweights = histlength+1; // inputs + 1 bias
+  threshold = 1.93 * histlength + 14;
+  //nperceptrons = (int)(predictor_size/(nweights*nbits)); // size / (nbits bits per weight * nweights)
+  nperceptrons = 413436;
+  weights = (int**) malloc(sizeof(int*)*nperceptrons);
+  for(int i=0;i<nperceptrons;i++)
+    weights[i] = (int*) malloc(sizeof(int)*nweights);
+
+  for(int i=0;i<nperceptrons;i++)
+  {
+    for(int j=0;j<nweights;j++)
+    {
+      weights[i][j] = 0;
+    }
+  }
+  history = (int8_t*) malloc(sizeof(int8_t)*histlength);
+  for(int i=0;i<histlength;i++)
+    history[i] = 0;
 }
 
-uint8_t make_custom_prediction(pc){
+uint32_t y;
+uint8_t make_custom_prediction(uint32_t pc){
+  int ppred = 0;
+  uint32_t idx;
+  int hist;
+  /*
 	// compute index
 	uint32_t percIdx=(pc & ((1<<pcIndexBits)-1)) % perceptron_num;
 	// calculate prediction y
-	uint32_t y = 0;
+	y = 0;
 	for(int i=0;i<=history_len;i++){
 		if(i != history_len) y += (history[i]>0)?weights[percIdx][i]:(-weights[percIdx][i]);
 		else y += weights[percIdx][i];
 	}
-	if(y>0) return TAKEN;
+	if(y>=0) return TAKEN;
 	else return NOTTAKEN;
+  */
+  idx = (pc & pcmask) % nperceptrons;
+  for(int i=0;i<histlength;i++)
+  {
+    hist = (history[i]>=0) ? 1:-1;
+    ppred += weights[idx][i] * hist;
+  }
+  ppred += weights[idx][histlength];
+  if(ppred>=0)
+    return TAKEN;
+  else
+    return NOTTAKEN;
 }
 
 void train_custom_predictor(uint32_t pc, uint8_t outcome){
-  uint32_t percIdx=(pc & ((1<<pcIndexBits)-1)) % perceptron_num;
+  //printf("hi");
+  //uint32_t percIdx=(pc & ((1<<pcIndexBits)-1)) % perceptron_num;
+  /*
   int y=0;
   for(int i=0;i<weight_num;i++){
     y+=(history[i]>0)?weights[percIdx][i]:(-weights[percIdx][i]);
   }
+  */
+  /*
   int out_sign=(outcome==TAKEN)?1:-1;
-  int y_sign=(y>=0)?1:-1
+  int y_sign=(y>=0)?1:-1;
   if(y_sign==out_sign || abs(y)<=threshold){
     for(int i=0;i<history_len;i++){
-      uint32_t
-      weights[percIdx][i]+=history[i]>=0?out_sign:(-out_sign);
+      //uint32_t
+      weights[percIdx][i]=weights[percIdx][i]+(history[i]>=0?out_sign:(-out_sign));
     }
     weights[percIdx][history_len]=out_sign;
   }
@@ -315,6 +391,37 @@ void train_custom_predictor(uint32_t pc, uint8_t outcome){
     history[i]=history[i+1];
   }
   history[history_len-1]=outcome;
+  return;
+  */
+  int ppred = 0;
+  uint32_t idx;
+  int hist;
+  int out;
+  uint8_t prediction = make_prediction(pc);
+  
+  idx = (pc & pcmask) % nperceptrons;
+  for(int i=0;i<nweights;i++)
+  {
+    hist = (history[i]>=0) ? 1:-1;
+    ppred += weights[idx][i] * hist;
+  }
+  if(prediction!=outcome || abs(ppred)<=threshold)
+  {
+    out = (outcome>0) ? 1:-1;
+    for(int i=0;i<histlength;i++)
+    {
+      hist = (history[i]>=0) ? 1:-1;
+      weights[idx][i] += out * hist;
+    }
+    weights[idx][histlength] = out;
+  }
+  for(int i=0;i<histlength-1;i++)
+  {
+    history[i] = history[i+1];
+  }
+  history[histlength-1] = outcome;
+  return;
+
 }
 //------------------------------------//
 //          Predictor Entry           //
